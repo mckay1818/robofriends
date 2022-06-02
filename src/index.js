10c65ae6a307c68d7a5e65543eb4a4c36029c5c3
mckay1818 <72710253+mckay1818@users.noreply.github.com>
2022-06-02 11:20:23 -0400

    init commit with hardcoded robofriend cards

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Card from './Card';
+import 'tachyons';
+//robots must be destructured since it is a named export
+import { robots } from './robots';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
   </React.StrictMode>
 );
 
